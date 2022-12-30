728365c7eedb2b85bf588718fabcfb1a2d3dd4f9
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-30 10:39:59 +0530

    9.2 : Candidates Applied Serializer ( serializer.py )

--- backend/job/serializers.py
+++ backend/job/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from job.models import Job
+from job.models import Job, CandidatesApplied
 
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+    
+
+
+class CandidatesAppliedSerializer(serializers.ModelSerializer):
+    job = JobSerializer()
+    
+    class Meta:
+        model = CandidatesApplied
+        fields = ("user", "resume", "job", "createdAt")
