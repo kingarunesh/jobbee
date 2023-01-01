61f78e4fe5d97dd3bfeb4a7aa5c3a709c156613b
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-01-01 23:27:57 +0530

    13.1 : context added ( AuthContext.js | _app.js )

---
+++ frontend/context/AuthContext.js
@@ -0,0 +1,12 @@
+import { useState, useEffect, createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [error, setError] = useState(null);
+
+    return <AuthContext.Provider value={{ loading, user, isAuthenticated, error }}>{children}</AuthContext.Provider>;
+};
