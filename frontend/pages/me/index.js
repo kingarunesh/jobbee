d1d42ff51b0de11ac452469d4417564ca9b11867
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-01-03 03:28:14 +0530

    14.3 : profile route protect ( pages/me/index.js | utils/isAuthenticated.js )

--- frontend/pages/me/index.js
+++ frontend/pages/me/index.js
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout/Layout";
 import UpdateProfile from "../../components/user/UpdateProfile";
+import { isAuthenticatedUser } from "../../utils/isAuthenticated";
 
 export default function UpdateProfilePage() {
     return (
@@ -9,4 +10,25 @@ export default function UpdateProfilePage() {
             </Layout>
         </>
     );
+}
+
+export async function getServerSideProps({ req }) {
+    const access_token = req.cookies.access;
+
+    const user = await isAuthenticatedUser(access_token);
+
+    if (!user) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            access_token,
+        },
+    };
 }
