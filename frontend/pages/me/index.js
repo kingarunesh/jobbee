3dd0ff16334ec846f9631f61546a625191d37e31
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-01-03 00:12:55 +0530

    14.1 : profile ( components/user/UpdateProfile.js | pages/me/index.js )

---
+++ frontend/pages/me/index.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout/Layout";
+import UpdateProfile from "../../components/user/UpdateProfile";
+
+export default function LoginPage() {
+    return (
+        <>
+            <Layout title="Profile">
+                <UpdateProfile />
+            </Layout>
+        </>
+    );
+}
