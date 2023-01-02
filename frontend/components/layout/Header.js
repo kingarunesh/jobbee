13906deb33193c9e3ff957cd493353007bbeda08
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-01-02 16:59:54 +0530

    13.6 : display user on header ( layout/Header.js )

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
 const Header = () => {
+    const { loading, user } = useContext(AuthContext);
+
     return (
         <>
             <div className="navWrapper">
@@ -22,11 +26,48 @@ const Header = () => {
                             </button>
                         </Link>
 
-                        <Link href="/login">
-                            <button className="loginButtonHeader">
-                                <span>Login</span>
-                            </button>
-                        </Link>
+                        {user ? (
+                            <div className="btn dropdown ml-3">
+                                <a
+                                    className="btn dropdown-toggle mr-4"
+                                    id="dropDownMenuButton"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                >
+                                    Hi, {user.first_name}
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="dropDownMenuButton">
+                                    <Link href="/employeer/jobs">
+                                        <span className="dropdown-item">My Jobs</span>
+                                    </Link>
+
+                                    <Link href="/me/applied">
+                                        <span className="dropdown-item">Jobs Applied</span>
+                                    </Link>
+
+                                    <Link href="/me">
+                                        <span className="dropdown-item">Profile</span>
+                                    </Link>
+
+                                    <Link href="/upload/resume">
+                                        <span className="dropdown-item">Upload Resume</span>
+                                    </Link>
+
+                                    <Link href="/">
+                                        <span className="dropdown-item text-danger">Logout</span>
+                                    </Link>
+                                </div>
+                            </div>
+                        ) : (
+                            !loading && (
+                                <Link href="/login">
+                                    <button className="loginButtonHeader">
+                                        <span>Login</span>
+                                    </button>
+                                </Link>
+                            )
+                        )}
                     </div>
                 </div>
             </div>
